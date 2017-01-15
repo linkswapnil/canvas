22b57677b3e68bdd77a70edfaa6ebbe664809e7e
linkswapnil <Shagun123>
2017-01-15 10:38:13 +0530

    Adding rectangle and circle controls

--- js/main.js
+++ js/main.js
@@ -5,4 +5,33 @@
 
     var canvasB = new fabric.Canvas('canvas2');
     canvasB.add(new fabric.Text("Canvas B", {left: 10, top: 20, lockMovementX: true, lockMovementY: true}));
+
+    var rectangleControl = document.getElementById('rectangle');
+    var circleControl = document.getElementById('circle');
+
+    rectangleControl.onclick = function () {
+        var rect = new fabric.Rect({
+            left: 60,
+            top: 70,
+            width: 150,
+            height: 75,
+            fill: 'transparent',
+            stroke: 'black',
+            strokeWidth: 2
+        });
+        canvasA.add(rect);
+    };
+
+    circleControl.onclick = function () {
+        var circle = new fabric.Circle({
+            left: 40,
+            top: 40,
+            radius: 50,
+            fill: 'transparent',
+            stroke: 'black',
+            strokeWidth: 2
+        });
+        canvasA.add(circle);
+    };
+
 })();
